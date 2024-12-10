495229296bdd26627b7d0887464e908ec9fa5aa6
Roman Gavrilov <roman.gavrilov@tktk.eegit config --global user.email roman.gavrilov@tktk.ee>
2024-12-10 22:53:58 +0200

    Week6

---
+++ Arrays/Program.cs
@@ -0,0 +1,25 @@
+﻿
+string[] fruitBasket = { "apples", "bananas", "oranges" };
+
+Console.WriteLine($"There are {fruitBasket.Length} fruit in your basket");
+
+for (int i = 0; i < fruitBasket.Length; i++)
+{
+    Console.WriteLine(fruitBasket[i]);
+}
+
+//Console.WriteLine(fruitBasket[0]);
+//Console.WriteLine(fruitBasket[1]);
+//Console.WriteLine(fruitBasket[2]);
+
+fruitBasket[0] = "pine-aplle";
+fruitBasket[2] = "peaches";
+
+//Console.WriteLine(fruitBasket[0]);
+//Console.WriteLine(fruitBasket[1]);
+//Console.WriteLine(fruitBasket[2]);
+
+for (int i = 0; i < fruitBasket.Length; i++)
+{
+    Console.WriteLine(fruitBasket[i]);
+}
---
+++ Week6StringsAndArrays/Program.cs
@@ -0,0 +1,32 @@
+﻿//Console.WriteLine("Enter your first name:");
+// string userName = Console.ReadLine().Trim();
+//string myString = "Hello, World!";
+//int theLenghtOfMyString = myString.Length;
+
+string userName = "Santa Klaus";
+Console.WriteLine(userName.Length);
+Console.WriteLine($"The last letter in your name is {userName[userName.Length - 1]}");
+//Console.WriteLine(userName[0]);
+//Console.WriteLine(userName[1]);
+//Console.WriteLine(userName[2]);
+//Console.WriteLine(userName[3]);
+//Console.WriteLine(userName[4]);
+
+// a-counter
+
+////int counter = 0;
+
+////for(int i = 0; i < userName.Length; i++)
+////{
+////    if (userName[i] == 'a')
+////        { counter++; } //counter = counter + 1
+
+////}
+////Console.WriteLine($"Your first name has {counter} a-letters.");
+//
+
+// replace
+// a - 4
+
+string updateUserName = userName.Replace('a', '4').Replace('l', '3');
+Console.WriteLine(updateUserName);
